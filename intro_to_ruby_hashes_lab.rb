2ebab890dd7c5e8eb4400a72addc69bfe81effad
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-26 00:41:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,15 +15,16 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  railroads: {pieces: 4},
+	  railroads: {pieces: 4,
 	  names: {},
 	  rent_in_dollars: {}
+	  }
 	}
 end
 
 def monopoly_with_third_tier
 	monopoly = {
-	  railroads: {pieces: 4},
+	  railroads: {pieces: 4,
 	  names: {
 	    reading_railroad: {},
 	    pennsylvania_railroad: {},
@@ -36,12 +37,14 @@ def monopoly_with_third_tier
 	    three_pieces_owned: 100,
 	    four_pieces_owned: 200
 	  }
+	  }
 	}
 end
 
 def monopoly_with_fourth_tier
 	monopoly = {
-	  railroads: {pieces: 4},
+	  railroads: {
+	    pieces: 4,
 	  names: {
 	    reading_railroad: {
 	      "mortgage_value" => "$100"
@@ -61,6 +64,7 @@ def monopoly_with_fourth_tier
 	    two_pieces_owned: 50,
 	    three_pieces_owned: 100,
 	    four_pieces_owned: 200
+	  }
 	  }
 	}
 end
